d2f0e125458b739e975b01c725adb068c70d90ef
Jan-Eric Nitschke <janericnitschke@gmail.com>
2022-01-03 21:44:31 +0100

    Now loops over all (both) supplied paths to demofile containing folders.

--- DemoAnalyzer_Sorter.py
+++ DemoAnalyzer_Sorter.py
@@ -5,9 +5,6 @@ import logging
 import shutil
 
 
-def ProduceLocations():
-    return
-
 Debug=True
 
 if Debug:
@@ -17,59 +14,67 @@ else:
 
 
 
-dir="D:\CSGO\Demos"
+dirs=["D:\CSGO\Demos","C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\\replays"]
 #dir="C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\\replays"
-StartID=118
-EndID=999
-logging.info("Scanning directory: "+dir)
+StartID=1
+EndID=99999
+
 existing_maps=["vertigo","ancient","cache","cbble","dust2","inferno","mirage","nuke","overpass","train","santorini_playtest","santorini"]
 logging.info("Maps considered: "+", ".join(existing_maps))
 
-logging.debug("Changing directoy now.")
-os.chdir(dir)
-#print(os.path.abspath(os.getcwd()))
 
-logging.info("Looping over demos in directory.")
-for filename in os.listdir(dir):
-    if filename.endswith(".dem"):
-        f = os.path.join(dir, filename)
-        # checking if it is a file
-        if os.path.isfile(f):
-            logging.info("At file: "+filename)
-            try:
-                int(filename[0:3])
-                ID=filename[0:3]
-            except ValueError:
-                ID=filename.split(".")[0]
-            logging.debug(("Using ID: "+ID))
-            if int(ID)>=StartID: #True and int(ID)<=EndID:
-                if int(ID)>EndID:
-                    logging.info("Parsed last relevant demo.")
-                    break
-                demo_parser = DemoParser(demofile=f,demo_id=ID,parse_rate=128, buy_style="hltv",dmg_rolled=True,parse_frames=True)
-                data = demo_parser.parse()
+
+#print(os.path.abspath(os.getcwd()))
+for dir in dirs:
+    os.chdir(dir)
+    logging.debug("Changing directoy now.")
+    logging.info("Scanning directory: "+dir)
+    logging.info("Looping over demos in directory.")
+    for filename in os.listdir(dir):
+        if filename.endswith(".dem"):
+            f = os.path.join(dir, filename)
+            # checking if it is a file
+            if os.path.isfile(f):
+                logging.info("At file: "+filename)
                 try:
-                    demo_parser.clean_rounds()
-                except AttributeError:
-                    pass
-                data=demo_parser.json
-                if data["mapName"].startswith("de_"):
-                    MapName=data["mapName"].split("_")[1]
-                else:
-                    MapName=data["mapName"]
-                logging.debug("Scanned map name: "+MapName)
-                if MapName not in existing_maps:
-                    logging.info("Map name "+MapName+" does not exist. Not moving json file to maps folder.")
-                    continue
-                source=os.path.join(dir,ID+".json")
-                destination="D:\CSGO\Demos\Maps\\" + MapName +"\\" + os.path.basename(f).split(".")[0]+".json"
-                logging.debug("Source: "+source)
-                logging.debug("Desitnation: "+destination)
-                try:
-                    os.rename(source, destination)
-                except FileExistsError:
-                    os.remove(destination)
-                    os.rename(source, destination)
-                except OSError:
-                    shutil.move(source, destination)
-                logging.info("Moved json to: "+destination)
+                    int(filename[0:3])
+                    ID=filename[0:3]
+                    NumberID=int(ID)
+                except ValueError:
+                    ID=filename.split(".")[0]
+                    NumberID=10000
+                logging.debug(("Using ID: "+ID))
+                if int(NumberID)>=StartID: #True and int(ID)<=EndID:
+                    if int(NumberID)>EndID:
+                        logging.info("Parsed last relevant demo.")
+                        break
+                    demo_parser = DemoParser(demofile=f,demo_id=ID,parse_rate=128, buy_style="hltv",dmg_rolled=True,parse_frames=True)
+                    data = demo_parser.parse()
+                    try:
+                        demo_parser.clean_rounds()
+                        pass
+                    except AttributeError:
+                        logging.info("This demo has an error while cleaning.")
+                        logging.exception('')
+                        pass
+                    data=demo_parser.json
+                    if data["mapName"].startswith("de_"):
+                        MapName=data["mapName"].split("_")[1]
+                    else:
+                        MapName=data["mapName"]
+                    logging.debug("Scanned map name: "+MapName)
+                    if MapName not in existing_maps:
+                        logging.info("Map name "+MapName+" does not exist. Not moving json file to maps folder.")
+                        continue
+                    source=os.path.join(dir,ID+".json")
+                    destination="D:\CSGO\Demos\Maps\\" + MapName +"\\" + os.path.basename(f).split(".")[0]+".json"
+                    logging.debug("Source: "+source)
+                    logging.debug("Desitnation: "+destination)
+                    try:
+                        os.rename(source, destination)
+                    except FileExistsError:
+                        os.remove(destination)
+                        os.rename(source, destination)
+                    except OSError:
+                        shutil.move(source, destination)
+                    logging.info("Moved json to: "+destination)
