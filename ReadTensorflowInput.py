ec7cae5551e0e8bbd45de906b74279ac4ed57344
Jan-Eric Nitschke <janericnitschke@gmail.com>
2022-01-26 20:59:20 +0100

    Read script now cleans the input. It can be chosen which side(s) to keep, how many timesteps should be considered each round and if the full coordinates or just the tokens should be used.

--- ReadTensorflowInput.py
+++ ReadTensorflowInput.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+from turtle import pos
 from csgo.analytics.nav import find_closest_area
 from csgo.parser import DemoParser
 import os
@@ -10,15 +13,122 @@ import sys
 from csgo.analytics import nav
 from csgo.data import NAV
 
-Debug=False
-if Debug:
-    logging.basicConfig(filename='D:\CSGO\ML\CSGOML\Test.log', encoding='utf-8', level=logging.DEBUG,filemode='w')
-else:
-    logging.basicConfig(filename='D:\CSGO\ML\CSGOML\Test.log', encoding='utf-8', level=logging.INFO,filemode='w')
+def transformToDataFrame(JsonFormatDict):
+    return pd.DataFrame(JsonFormatDict)
 
-with open("D:\CSGO\ML\CSGOML\Prepared_Input_Tensorflow.json", encoding='utf-8') as PreAnalyzed:
-    dataframe=pd.read_json(PreAnalyzed)
-    round_trajectory=pd.DataFrame(dataframe.iloc[30]["position_df"])
-    logging.info(dataframe)
-    logging.info(round_trajectory)
+
+def GetMinMaxFromFirst(reference_position_df,team):
+    minimum={"x":sys.maxsize,"y":sys.maxsize,"z":sys.maxsize}
+    maximum={"x":-sys.maxsize,"y":-sys.maxsize,"z":-sys.maxsize}
+    if team=="BOTH":
+        sides=["CT","T"]
+    else:
+        sides=[team]
+    for feature in ["x","y","z"]:
+        for side in sides:
+            for number in range(1,6):
+                maximum[feature]=max(reference_position_df[side+"Player"+str(number)+feature].max(),maximum[feature])
+                minimum[feature]=min(reference_position_df[side+"Player"+str(number)+feature].min(),minimum[feature])
+    return minimum,maximum
+
+def RegularizeCoordinates(Coordinate,minimum,maximum):
+    mean=(minimum+maximum)/2
+    dev=(maximum-minimum)/2
+    return (Coordinate-mean)/dev
     
+
+def modifyDataFrameShape(position_df,team,time,coordinates,minimum,maximum):
+    if coordinates:
+        ColumnsToKeep=[]
+        if team=="BOTH":
+            sides=["CT","T"]
+        else:
+            sides=[team]
+        for side in sides:
+            for number in range(1,6):
+                for feature in ["Alive","x","y","z"]:
+                    ColumnsToKeep.append(side+"Player"+str(number)+feature)
+                    if not feature=="Alive":
+                        position_df[side+"Player"+str(number)+feature]=position_df[side+"Player"+str(number)+feature].apply(RegularizeCoordinates,args=(minimum[feature],maximum[feature]))
+        position_df=position_df[ColumnsToKeep]
+
+        # Still need to regularize the coordinates
+    else:
+        # Remove all columns from the dataframe except for the column of interest
+        if team=="BOTH":
+            position_df=position_df[["token"]]
+        else:
+            position_df=position_df[[team+"token"]]
+    # Set length of dataframe to make sure all have the same size
+    # Pad each column with its last entry if set size is larger than dataframe size
+    if time>len(position_df):
+        idx = np.minimum(np.arange(time), len(position_df) - 1) 
+        position_df=position_df.iloc[idx]
+    else:
+        # Cut if the required size is smaller.
+        position_df=position_df.head(time)
+    position_df.reset_index(drop=True, inplace=True)
+    return position_df
+
+def getMaximumLength(position_dfs):
+    lengths=position_dfs["position_df"].apply(len)
+    return lengths.max()
+
+        
+
+def main(args):
+    parser = argparse.ArgumentParser("Analyze the early mid fight on inferno")
+    parser.add_argument("-d", "--debug",  action='store_true', default=False, help="Enable debug output.")
+    parser.add_argument("-m", "--map",  default="overpass", help="Map to analyze")
+    parser.add_argument("-l", "--log",  default='D:\CSGO\ML\CSGOML\Test.log', help="Path to output log.")
+    parser.add_argument("-s", "--side",  default='BOTH', help="Which side to include in analysis (CT,T,BOTH) .")
+    parser.add_argument("--coordinates",  action='store_true', default=False, help="Whether to use full coordinate or just tokens.")
+    parser.add_argument("-t", "--time",  default='MAX', help="How many position snapshots should be used. Either an integer or MAX.")
+    options = parser.parse_args(args)
+
+    if options.debug:
+        logging.basicConfig(filename=options.log, encoding='utf-8', level=logging.DEBUG,filemode='w')
+    else:
+        logging.basicConfig(filename=options.log, encoding='utf-8', level=logging.INFO,filemode='w')
+
+    if options.side not in ["CT","T","BOTH"]:
+        logging.info("Side "+options.side+" unknown. Has to be one of 'CT','T','BOTH'!")
+        sys.exit
+
+    try:
+        int(options.time)
+    except ValueError:
+        if  not options.time =="MAX":
+            logging.info("Time "+options.time+" unknown. Has to be either an integer or 'MAX'!")
+            sys.exit
+
+    minimum={}
+    maximum={}
+    File="D:\CSGO\Demos\Maps\\"+options.map+"\Analysis\Prepared_Input_Tensorflow_"+options.map+".json"
+    if os.path.isfile(File):
+        with open(File, encoding='utf-8') as PreAnalyzed:
+            dataframe=pd.read_json(PreAnalyzed)
+            logging.info("Initial dataframe:")
+            logging.info(dataframe)
+            dataframe=dataframe[["Winner","position_df"]]
+            dataframe["position_df"]=dataframe["position_df"].apply(transformToDataFrame)
+            logging.info("Example for position_df dataframe entry before cleaning.")
+            logging.info(dataframe.iloc[30]["position_df"])
+            if options.time =="MAX":
+                time=getMaximumLength(dataframe)
+            else:
+                time=int(options.time)
+            logging.info("Time has been set to "+str(time)+"!")
+            if options.coordinates:
+                minimum,maximum=GetMinMaxFromFirst(dataframe.iloc[0]["position_df"],options.side)
+            dataframe["position_df"]=dataframe["position_df"].apply(modifyDataFrameShape,args=(options.side,time,options.coordinates,minimum,maximum))
+            logging.info("Dataframe after cleanup")
+            logging.info(dataframe)
+            logging.info("Example for position_df dataframe entry after cleaning.")
+            logging.info(dataframe.iloc[30]["position_df"])
+    else:
+        logging.info("File "+File+" does not exist! Probably because map "+options.map+" does not exist!")
+        
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
