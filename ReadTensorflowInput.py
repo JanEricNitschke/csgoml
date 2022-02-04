612ec7a4a14da3646a5b1a49f2d2fd92154832ca
Jan-Eric Nitschke <janericnitschke@gmail.com>
2022-02-04 20:50:28 +0100

    Added actual tensorflow model building. Adjusted inputs.

--- ReadTensorflowInput.py
+++ ReadTensorflowInput.py
@@ -9,6 +9,8 @@ import numpy as np
 import argparse
 import sys
 import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers 
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
@@ -20,8 +22,7 @@ def PadToLength(position_df,time):
         # Pad with last entry
         #idx = np.minimum(np.arange(time), len(position_df) - 1)
         #position_df=position_df.iloc[idx]
-        # Pad with 0
-        position_df=position_df.reindex(range(time), fill_value=0.0)
+        position_df=position_df.reindex(range(time), fill_value=0.0, method="pad")
     else:
         # Cut if the required size is smaller.
         position_df=position_df.head(time)
@@ -38,13 +39,14 @@ def GenerateArrayForTokens(position_df,token,time):
     # Convert the dataframe to a list and back into a df to have one column for each entry of the string
     position_df=pd.DataFrame(position_df.tolist())
     # Convert the individual string into the respective integers
-    position_df.apply(pd.to_numeric)
+    # Divide numbers by 5 so they are all from 0 to 1
+    position_df.applymap(lambda x: float(x)/5)
     # Pad the df to the specified length
     position_df=PadToLength(position_df,time)
     # Convert to numpy array
-    return_array=position_df.to_numpy()
-    # for ind, column in enumerate(position_df.columns):
-    #     return_array[:,ind]=position_df[column].to_numpy()
+    #return_array=position_df.to_numpy()
+    for ind, column in enumerate(position_df.columns):
+        return_array[:,ind]=position_df[column].to_numpy()
     return return_array
 
 
@@ -57,9 +59,10 @@ def modifyDataFrameShape(position_df,team,time,coordinates):
     # Pad each column if set size is larger than dataframe size
     # If the full coordinates should be used then for each player their alive status as well as their x,y,z coordinates are kept, everything else (player names and tokens) is discarded.
     if coordinates:
-        dimensions=[time]
+        featurelist=["x","y","z"]
+        #featurelist=["Alive","x","y","z"]
+        dimensions=[time,5,len(featurelist)] # time for timesteps, 5 for players, len(feautrelist) for features
         position_df=PadToLength(position_df,time)
-        featurelist=["Alive","x","y","z"]
         if team=="BOTH":
             sides=["CT","T"]
             # Dimension with size 2 for teams
@@ -68,16 +71,13 @@ def modifyDataFrameShape(position_df,team,time,coordinates):
             sides=[team]
             # Dimension with size 1 for team
             dimensions.append(1)
-        # Dimension with size 5 for players
-        dimensions.append(5)
-        # Dimensions with size 4 for features
-        dimensions.append(4)
+
         # Only keep the information for the side(s) that should be used.
         return_array=np.zeros(tuple(dimensions))
         for side in sides:
             for number in range(1,6):
                 for feature in featurelist:
-                    return_array[:,sides.index(side),number-1,featurelist.index(feature)]=position_df[side+"Player"+str(number)+feature].to_numpy()
+                    return_array[:,number-1,featurelist.index(feature),sides.index(side)]=position_df[side+"Player"+str(number)+feature].to_numpy()
     else:
         # Remove all columns from the dataframe except for the column of interest
         # Split the token strings into a columns so that there is one column for each integer in the token string
@@ -98,17 +98,45 @@ def TransFormDataframeToArrays(dataframe):
         feature_array[index]=array
     return label_array, feature_array
 
+def GetTokenModel(NodesPerLayer,input_shape):
+    model = tf.keras.Sequential([
+        layers.LSTM(NodesPerLayer,input_shape=input_shape),
+        layers.Dense(NodesPerLayer, activation='relu'), #,kernel_regularizer=tf.keras.regularizers.l2(0.001)
+        #layers.Dropout(0.4),
+        layers.Dense(NodesPerLayer, activation='relu'), #,kernel_regularizer=tf.keras.regularizers.l2(0.001)
+        #layers.Dropout(0.4),
+        layers.Dense(1)
+        ])
+    return model
+
+def GetCoordinateModel(NodesPerLayer,input_shape):
+    pooling_size=(2,1)
+    model = tf.keras.Sequential([
+        layers.TimeDistributed(layers.Conv2D(NodesPerLayer/2,pooling_size,activation='elu',padding="same"),input_shape=input_shape),
+        layers.TimeDistributed(layers.AveragePooling2D(pool_size=pooling_size,strides=(1,1))),
+        layers.TimeDistributed(layers.Conv2D(NodesPerLayer,pooling_size,activation='elu',padding="same"),input_shape=input_shape),
+        layers.TimeDistributed(layers.AveragePooling2D(pool_size=pooling_size,strides=(1,1))),
+        layers.TimeDistributed(layers.Flatten()),
+        layers.LSTM(NodesPerLayer),
+        layers.Dense(NodesPerLayer, activation='relu'), #,kernel_regularizer=tf.keras.regularizers.l2(0.001)
+        layers.Dropout(0.4),
+        layers.Dense(NodesPerLayer, activation='relu'), #,kernel_regularizer=tf.keras.regularizers.l2(0.001)
+        layers.Dropout(0.4),
+        layers.Dense(1)
+        ])
+    return model
+
 
 def main(args):
     parser = argparse.ArgumentParser("Analyze the early mid fight on inferno")
     parser.add_argument("-d", "--debug",  action='store_true', default=False, help="Enable debug output.")
-    parser.add_argument("-m", "--map",  default="overpass", help="Map to analyze")
+    parser.add_argument("-m", "--map",  default="mirage", help="Map to analyze")
     parser.add_argument("-l", "--log",  default='D:\CSGO\ML\CSGOML\Test.log', help="Path to output log.")
     parser.add_argument("-s", "--side",  default='BOTH', help="Which side to include in analysis (CT,T,BOTH) .")
     parser.add_argument("--coordinates",  action='store_true', default=False, help="Whether to use full coordinate or just tokens.")
     parser.add_argument("-t", "--time",  default='MAX', help="How many position snapshots should be used. Either an integer or MAX.")
     parser.add_argument("--exampleid", type=int, default=22, help="For which round the position_df should be printed when debugging.")
-    parser.add_argument("--randomstate", type=int, default=8, help="Random state for train_test_split")
+    parser.add_argument("--randomstate", type=int, default=123, help="Random state for train_test_split")
     options = parser.parse_args(args)
 
     if options.debug:
@@ -180,89 +208,81 @@ def main(args):
     val_labels, val_features = TransFormDataframeToArrays(val_df)
     train_labels, train_features = TransFormDataframeToArrays(train_df)
 
-    logging.info(test_features)
-    logging.info(test_features.shape)
+    logging.info("Input preparation done!")
+    logging.info(train_labels.shape)
+    logging.info(train_features.shape)
+
+    train_dataset = tf.data.Dataset.from_tensor_slices((train_features, train_labels))
+    test_dataset = tf.data.Dataset.from_tensor_slices((test_features, test_labels))
+    val_dataset = tf.data.Dataset.from_tensor_slices((val_features, val_labels))
+
+    BATCH_SIZE = 32
+    SHUFFLE_BUFFER_SIZE = 40
+
+    train_dataset = train_dataset.batch(BATCH_SIZE)
+    test_dataset = test_dataset.batch(BATCH_SIZE)
+    val_dataset = val_dataset.batch(BATCH_SIZE)
+    NodesPerLayer=32
 
 
+    if options.coordinates:
+        model=GetCoordinateModel(NodesPerLayer,train_features[0].shape)
+    else:
+        model=GetTokenModel(NodesPerLayer,train_features[0].shape)
 
-    # logging.info("Input preparation done!")
-    # logging.info(train_labels.shape)
-    # logging.info(train_features.shape)
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.00007),
+        loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+        metrics=[
+            "accuracy",
+            tf.keras.losses.BinaryCrossentropy(
+                from_logits=True, name='binary_crossentropy'),
+                ])
 
-    # train_dataset = tf.data.Dataset.from_tensor_slices((train_features, train_labels))
-    # test_dataset = tf.data.Dataset.from_tensor_slices((test_features, test_labels))
-    # val_dataset = tf.data.Dataset.from_tensor_slices((val_features, val_labels))
+    model.summary()
 
-    # BATCH_SIZE = 64
-    # SHUFFLE_BUFFER_SIZE = 100
+    history = model.fit(
+        train_dataset,
+        epochs=50,
+        batch_size=BATCH_SIZE,
+        validation_data=val_dataset,
+        callbacks=tf.keras.callbacks.EarlyStopping(monitor='val_binary_crossentropy', patience=5),
+        )
 
-    # train_dataset = train_dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
-    # test_dataset = test_dataset.batch(BATCH_SIZE)
-    # val_dataset = val_dataset.batch(BATCH_SIZE)
+    loss, accuracy, entropy = model.evaluate(test_dataset)
 
-    # model = tf.keras.Sequential([
-    #     tf.keras.layers.Masking(mask_value=0.,input_shape=train_features[0].shape)
-    #     tf.keras.layers.Dense(16, activation='relu'),
-    #     tf.keras.layers.Dropout(0.5),
-    #     tf.keras.layers.Dense(16, activation='relu'),
-    #     tf.keras.layers.Dropout(0.5),
-    #     #tf.keras.layers.Dense(1, activation="sigmoid")
-    #     tf.keras.layers.Dense(1)
-    # ])
+    print("Loss: ", loss)
+    print("Accuracy: ", accuracy)
 
-    # model.compile(
-    #     optimizer='adam',
-    #     loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-    #     metrics=[
-    #         tf.metrics.BinaryAccuracy(threshold=0.0),
-    #         tf.keras.losses.BinaryCrossentropy(
-    #             from_logits=True, name='binary_crossentropy'),
-    #             ])
+    history_dict = history.history
 
-    # model.summary()
+    acc = history_dict['accuracy']
+    val_acc = history_dict['val_accuracy']
+    loss = history_dict['loss']
+    val_loss = history_dict['val_loss']
 
-    # history = model.fit(
-    #     train_dataset,
-    #     epochs=15,
-    #     batch_size=BATCH_SIZE,
-    #     validation_data=val_dataset,
-    #     callbacks=tf.keras.callbacks.EarlyStopping(monitor='val_binary_crossentropy', patience=200),
-    #     )
+    epochs = range(1, len(acc) + 1)
 
-    # loss, accuracy = model.evaluate(test_dataset)
+    # "bo" is for "blue dot"
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    # b is for "solid blue line"
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
 
-    # print("Loss: ", loss)
-    # print("Accuracy: ", accuracy)
-
-    # history_dict = history.history
-
-    # acc = history_dict['binary_accuracy']
-    # val_acc = history_dict['val_binary_accuracy']
-    # loss = history_dict['loss']
-    # val_loss = history_dict['val_loss']
-
-    # epochs = range(1, len(acc) + 1)
-
-    # # "bo" is for "blue dot"
-    # plt.plot(epochs, loss, 'bo', label='Training loss')
-    # # b is for "solid blue line"
-    # plt.plot(epochs, val_loss, 'b', label='Validation loss')
-    # plt.title('Training and validation loss')
-    # plt.xlabel('Epochs')
-    # plt.ylabel('Loss')
-    # plt.legend()
-
-    # plt.show()
+    plt.show()
 
 
-    # plt.plot(epochs, acc, 'bo', label='Training acc')
-    # plt.plot(epochs, val_acc, 'b', label='Validation acc')
-    # plt.title('Training and validation accuracy')
-    # plt.xlabel('Epochs')
-    # plt.ylabel('Accuracy')
-    # plt.legend(loc='lower right')
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend(loc='lower right')
 
-    # plt.show()
+    plt.show()
 
     # #input_tensor=tf.convert_to_tensor(dataframe)
     # #logging.info(input_tensor)
