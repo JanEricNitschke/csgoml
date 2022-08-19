a91227d6d6e247e3a9396c82c8d33e2c2163c758
Jan-Eric Nitschke <jan-eric@nitschke.tk>
2022-08-19 13:48:35 +0200

    Added script to precompute distance matrices between tiles and areas for each map.

--- README.md
+++ README.md
@@ -9,21 +9,19 @@ Useful when you have accumulated a large collection of your own demos and/or whe
 # fight_analyzer.py
 Contains a class for analyzing a specifically defined engagement for whether it is T or CT favoured.
 
-Running the script analyzes the early (first 5 to 25 seconds of the round) mid fight on inferno on gun rounds.
+Running the script adds every kill as well as information about the map,  players locations and weapons as well as the time to a MySQL database.
 
-For that kill events are analyzed for their time, attacker and victim position and weaponry.
+It also supports the ability to query specific situations (by map, player weapons and positions and game time) for their CT win percentage.
 
-For each qualifying event an entry is made into a dataframe containing information about the time,round,matchid,killing weapon, winner and areas for further analysis.
-
-The class natively supports checking the CT win percentage for different allowed time windows (One second steps from min to max. So just events between 5 and 6 seconds, 5 and 7, up to all 5 to 25).
+The latter functionality has also been given a front-end either [here](https://github.com/JanEricNitschke/CSGOML/tree/main/AWS_Steps/FightAnalyzer) or updated [here](https://github.com/JanEricNitschke/AngularFightAnalyzer)
 
 # tensorflow_input_preparation.py
-A script that produces, for each map separately, a json file containing different configurations of player trajectory data for each round played on that map. 
+A script that produces, for each map separately, a json file containing different configurations of player trajectory data for each round played on that map.
 
 This is in perparation of further analysis to separate the extensive cleaning neccessary from the final analysis.
 
 # read_tensorflow_input.py
-Contains a class designed to read in the json files produced by tensorflow_input_preparation.py and train LSTM networks to predict a winner of a round based on player trajectory data. 
+Contains a class designed to read in the json files produced by tensorflow_input_preparation.py and train LSTM networks to predict a winner of a round based on player trajectory data.
 
 It supports the option to chose between which side(s) to consider, limit the data to only contain the first n seconds and to chose
 between using each players full x, y and z coordinates or a tokenized version as described in [ggViz: Accelerating Large-Scale Esports Game Analysis](https://arxiv.org/pdf/2107.06495.pdf) and implemented in [awpy](https://github.com/pnxenopoulos/awpy).
@@ -31,17 +29,23 @@ between using each players full x, y and z coordinates or a tokenized version as
 # download_demos.py and demo_watchdog.py
 Two scripts used to build a dataset large enough to enable machine learning techniques to fulfill their potential.
 
-download_demos.py downloads the demos from professional CS:GO games tracked on [hltv](https://www.hltv.org/). 
+download_demos.py downloads the demos from professional CS:GO games tracked on [hltv](https://www.hltv.org/).
 
 demo_watchdog.py then unpacks the resulting rar file
-and calls demo_analyzer_sort.py to parse the demos to json files and store them based on the map played. 
+and calls demo_analyzer_sort.py to parse the demos to json files and store them based on the map played.
 
-The full demos is subsequently deleted as hard disk requirement needed to store all demos in full us currently infeasible for me. 
+The full demos is subsequently deleted as hard disk requirement needed to store all demos in full us currently infeasible for me.
 
 Currently more than 1000 matches (>2000 maps with over 50000 rounds) have been accumulated.
 
 # plot_utils.py
 
-This is a module containing various functions that augments already existing plotting functions present in [awpy](https://github.com/pnxenopoulos/awpy).
+This is a module containing various functions that augment already existing plotting functions present in [awpy](https://github.com/pnxenopoulos/awpy).
 Specifically the plotting of position tokens and visualization of named areas.
 Run as a script it illustrates the basic functionality of these functions as well as the basic ones directly from awpy.
+
+# nav_utils.py
+This is a module containing functions augmenting navigation capabilities of [awpy](https://github.com/pnxenopoulos/awpy).
+It contains functions to precompute distance matricesfor both tiles and regions of each map.
+Included are also plotting functions to validate the results.
+The matrices for the regions are included in this repo while those for the tiles are too large and thus stored separately [here](https://cernbox.cern.ch/index.php/s/oUW69uhH9JLBMh2).
---
+++ nav_utils.py
@@ -0,0 +1,502 @@
+"""Collection of functions to extend awpy's navigation capabilities
+
+
+    Typical usage example:
+
+    area_matrix = create_area_distance_matrix()
+    logging.info(area_matrix["de_dust2"]["ExtendedA"]["CTSpawn"])
+    logging.info(area_matrix["de_dust2"]["CTSpawn"]["ExtendedA"])
+
+    centroids, reps = generate_centroids("de_dust2")
+    graph = area_distance(
+        "de_dust2", centroids["ExtendedA"], centroids["CTSpawn"], dist_type="graph"
+    )
+    geodesic = area_distance(
+        "de_dust2",  centroids["ExtendedA"], centroids["CTSpawn"], dist_type="geodesic"
+    )
+    plot_path("de_dust2", graph, geodesic)
+    graph = area_distance(
+        "de_dust2", centroids["CTSpawn"], centroids["ExtendedA"], dist_type="graph"
+    )
+    geodesic = area_distance(
+        "de_dust2", centroids["CTSpawn"],  centroids["ExtendedA"], dist_type="geodesic"
+    )
+    plot_path("de_dust2", graph, geodesic)
+    graph = area_distance(
+        "de_dust2", reps["ExtendedA"], reps["CTSpawn"], dist_type="graph"
+    )
+    geodesic = area_distance(
+        "de_dust2",  reps["ExtendedA"], reps["CTSpawn"], dist_type="geodesic"
+    )
+    plot_path("de_dust2", graph, geodesic)
+    graph = area_distance(
+        "de_dust2", reps["CTSpawn"], reps["ExtendedA"], dist_type="graph"
+    )
+    geodesic = area_distance(
+        "de_dust2", reps["CTSpawn"],  reps["ExtendedA"], dist_type="geodesic"
+    )
+    plot_path("de_dust2", graph, geodesic)
+
+
+"""
+#!/usr/bin/env python
+
+from collections import defaultdict
+import itertools
+import os
+import sys
+import math
+import logging
+import json
+import argparse
+from statistics import mean, median
+import numpy as np
+from shapely.geometry import Polygon
+from matplotlib import patches
+import matplotlib.pyplot as plt
+from awpy.visualization.plot import plot_map, position_transform, tree
+from awpy.data import NAV
+from awpy.analytics.nav import area_distance, find_closest_area
+from plotting_utils import get_areas_hulls_centers, stepped_hull
+
+
+def mark_areas(map_name, areas):
+    """Plots the given map and marks the tiles of the two given areas.
+
+    Args:
+        map_name (string): Map to plot
+        area (list[int]): List of ids of the areas to highlight on the map
+
+    Returns:
+        None (Directly saves the plot to file)
+    """
+    fig, axis = plot_map(map_name=map_name, map_type="simplerader", dark=True)
+    fig.set_size_inches(19.2, 10.8)
+    for a in NAV[map_name]:
+        area = NAV[map_name][a]
+        if a not in areas:
+            continue
+        try:
+            south_east_x = position_transform(map_name, area["southEastX"], "x")
+            north_west_x = position_transform(map_name, area["northWestX"], "x")
+            south_east_y = position_transform(map_name, area["southEastY"], "y")
+            north_west_y = position_transform(map_name, area["northWestY"], "y")
+        except KeyError:
+            pass
+        # Get its lower left points, height and width
+        width = south_east_x - north_west_x
+        height = north_west_y - south_east_y
+        southwest_x = north_west_y
+        southwest_y = south_east_y
+        rect = patches.Rectangle(
+            (southwest_x, southwest_y),
+            width,
+            height,
+            linewidth=1,
+            edgecolor="red",
+            facecolor="None",
+        )
+        axis.add_patch(rect)
+    plt.savefig(
+        f"D:\\CSGO\\ML\\CSGOML\\Plots\\distance_matrix_fails\\{map_name}_{'_'.join(areas)}.png",
+        bbox_inches="tight",
+        dpi=1000,
+    )
+    fig.clear()
+    plt.close(fig)
+
+
+def plot_path(map_name, graph, geodesic):
+    """Plots the given map and two paths between two areas.
+
+    Args:
+        map_name (string): Map to plot
+        graph (dict): Distance dictionary. The graph["distance"] is the distance between two areas.
+                      graph["areas"] is a list of int area ids in the shortest path between the first and last entry.
+        geodesic (dict): Distance dictionary. The geodesic["distance"] is the distance between two areas.
+                      geodesic["areas"] is a list of int area ids in the shortest path between the first and last entry.
+
+    Returns:
+        None (Directly saves the plot to file)
+    """
+    fig, axis = plot_map(map_name=map_name, map_type="simplerader", dark=True)
+    fig.set_size_inches(19.2, 10.8)
+    for a in NAV[map_name]:
+        area = NAV[map_name][a]
+        if a not in graph["areas"] and a not in geodesic["areas"]:
+            continue
+        try:
+            south_east_x = position_transform(map_name, area["southEastX"], "x")
+            north_west_x = position_transform(map_name, area["northWestX"], "x")
+            south_east_y = position_transform(map_name, area["southEastY"], "y")
+            north_west_y = position_transform(map_name, area["northWestY"], "y")
+        except KeyError:
+            pass
+        # Get its lower left points, height and width
+        width = south_east_x - north_west_x
+        height = north_west_y - south_east_y
+        southwest_x = north_west_x
+        southwest_y = south_east_y
+        color = "yellow"
+        if a in graph["areas"] and a in geodesic["areas"]:
+            color = "green"
+        elif a in graph["areas"]:
+            color = "red"
+        elif a in geodesic["areas"]:
+            color = "blue"
+        if graph["areas"] and a == graph["areas"][-1]:
+            color = "orange"
+        if graph["areas"] and a == graph["areas"][0]:
+            color = "purple"
+        rect = patches.Rectangle(
+            (southwest_x, southwest_y),
+            width,
+            height,
+            linewidth=1,
+            edgecolor=color,
+            facecolor="None",
+        )
+        axis.add_patch(rect)
+    plt.savefig(
+        f"D:\\CSGO\\ML\\CSGOML\\Plots\\distance_matrix_examples\\{map_name}_{graph[0]}_{graph[-1]}_{graph['distance']}_{geodesic['distance']}.png",
+        bbox_inches="tight",
+        dpi=1000,
+    )
+    fig.clear()
+    plt.close(fig)
+
+
+def plot_mid(map_name):
+    """Plots the given map with the hulls, centroid and a representative_point for each named area.
+
+    Args:
+        map_name (string): Map to plot
+
+    Returns:
+        None (Directly saves the plot to file)
+    """
+    cent_ids, rep_ids = generate_centroids(map_name)
+    output_path = r"D:\\CSGO\\ML\\CSGOML\\Plots\\"
+    fig, axis = plot_map(map_name=map_name, map_type="simpleradar", dark=True)
+    fig.set_size_inches(19.2, 10.8)
+    # Grab points, hull and centroid for each named area
+    area_points, hulls, _ = get_areas_hulls_centers(map_name)
+    for area in area_points:
+        # Dont plot the "" area as it stretches across the whole map and messes with clarity
+        # but add it to the legend
+        if not area:
+            axis.plot(np.NaN, np.NaN, label="None")
+            continue
+        axis.plot(hulls[area][:, 0], hulls[area][:, 1], "-", lw=3, label=area)
+    handles, labels = axis.get_legend_handles_labels()
+    lgd = axis.legend(handles, labels, loc="upper left", bbox_to_anchor=(1.01, 1.01))
+    for a in NAV[map_name]:
+        area = NAV[map_name][a]
+        if a not in cent_ids.values() and a not in rep_ids.values():
+            continue
+        logging.info(a)
+        logging.info(NAV["de_dust2"][a])
+        try:
+            south_east_x = position_transform(map_name, area["southEastX"], "x")
+            north_west_x = position_transform(map_name, area["northWestX"], "x")
+            south_east_y = position_transform(map_name, area["southEastY"], "y")
+            north_west_y = position_transform(map_name, area["northWestY"], "y")
+        except KeyError:
+            pass
+        # Get its lower left points, height and width
+        width = south_east_x - north_west_x
+        height = north_west_y - south_east_y
+        southwest_x = north_west_x
+        southwest_y = south_east_y
+        color = "yellow"
+        if a in cent_ids.values() and a in rep_ids.values():
+            color = "green"
+        elif a in cent_ids.values():
+            color = "red"
+        elif a in rep_ids.values():
+            color = "blue"
+        rect = patches.Rectangle(
+            (southwest_x, southwest_y),
+            width,
+            height,
+            linewidth=1,
+            edgecolor=color,
+            facecolor="None",
+        )
+        axis.add_patch(rect)
+    plt.savefig(
+        os.path.join(output_path, f"test_{map_name}.png"),
+        bbox_inches="tight",
+        bbox_extra_artists=(lgd,),
+        dpi=1000,
+    )
+    fig.clear()
+    plt.close(fig)
+
+
+def create_tile_distance_matrix(save=False):
+    """Creates a tree like nested dictionary containing distance matrices (as dicts) for each map for all tiles
+
+    Args:
+        save (boolean): Whether to also save the dictionary as a json file
+
+    Returns:
+        Tree structure containing distances for all tile pairs on all maps
+    """
+    json_path = "D:\\CSGO\\ML\\CSGOML\\data\\tile_dist_matrix.json"
+    if os.path.exists(json_path):
+        with open(json_path, encoding="utf8") as f:
+            tile_distance_matrix = json.load(f)
+        return tile_distance_matrix
+    tile_distance_matrix = {
+        "euclidean": tree(),
+        "graph": tree(),
+        "geodesic": tree(),
+    }
+    for map_name, tiles in NAV.items():
+        logging.debug(map_name)
+        for area1 in tiles:
+            area1 = int(area1)
+            area1_x = (
+                NAV[map_name][area1]["southEastX"] + NAV[map_name][area1]["northWestX"]
+            ) / 2
+            area1_y = (
+                NAV[map_name][area1]["southEastY"] + NAV[map_name][area1]["northWestY"]
+            ) / 2
+            area1_z = (
+                NAV[map_name][area1]["southEastZ"] + NAV[map_name][area1]["northWestZ"]
+            ) / 2
+            for area2 in tiles:
+                area2 = int(area2)
+                area2_x = (
+                    NAV[map_name][area2]["southEastX"]
+                    + NAV[map_name][area2]["northWestX"]
+                ) / 2
+                area2_y = (
+                    NAV[map_name][area2]["southEastY"]
+                    + NAV[map_name][area2]["northWestY"]
+                ) / 2
+                area2_z = (
+                    NAV[map_name][area2]["southEastZ"]
+                    + NAV[map_name][area2]["northWestZ"]
+                ) / 2
+                tile_distance_matrix["euclidean"][map_name][area1][area2] = math.sqrt(
+                    (area1_x - area2_x) ** 2
+                    + (area1_y - area2_y) ** 2
+                    + (area1_z - area2_z) ** 2
+                )
+                try:
+                    graph = area_distance(map_name, area1, area2, dist_type="graph")
+                    tile_distance_matrix["graph"][map_name][area1][area2] = graph[
+                        "distance"
+                    ]
+                    geodesic = area_distance(
+                        map_name, area1, area2, dist_type="geodesic"
+                    )
+                    tile_distance_matrix["geodesic"][map_name][area1][area2] = geodesic[
+                        "distance"
+                    ]
+                except Exception as e:
+                    logging.warning(
+                        "Exception '%s' occured while trying to find path between area %s in region %s and area %s in region %s",
+                        e,
+                        area1,
+                        tiles[area1]["areaName"],
+                        area2,
+                        tiles[area2]["areaName"],
+                    )
+                    tile_distance_matrix["graph"][map_name][area1][area2] = float("inf")
+                    tile_distance_matrix["geodesic"][map_name][area1][area2] = float(
+                        "inf"
+                    )
+    if save:
+        with open(json_path, "w", encoding="utf8") as json_file:
+            json.dump(tile_distance_matrix, json_file)
+    return tile_distance_matrix
+
+
+def create_area_distance_matrix(tile_distance_matrix=None, save=False):
+    """Creates a tree like nested dictionary containing distance matrices (as dicts) for each map for all regions
+
+    Args:
+        tile_distance_matrix (tree): A tree like structure containing the distances between all tile combinations
+        save (boolean): Whether to also save the dictionary as a json file
+
+    Returns:
+        Tree structure containing distances for all tile pairs on all maps
+    """
+    json_path = "D:\\CSGO\\ML\\CSGOML\\data\\area_dist_matrix.json"
+    if os.path.exists(json_path):
+        with open(json_path, encoding="utf8") as f:
+            area_distance_matrix = json.load(f)
+        return area_distance_matrix
+    area_distance_matrix = tree()
+    if tile_distance_matrix is None:
+        tile_distance_matrix = create_tile_distance_matrix()
+    for dist_type in tile_distance_matrix:
+        logging.debug("Dist_type: %s", dist_type)
+        for map_name, tiles in NAV.items():
+            logging.debug("Map_name: %s", map_name)
+            area_mapping = defaultdict(list)
+            for area in tiles:
+                area_mapping[tiles[area]["areaName"]].append(area)
+            centroids, reps = generate_centroids(map_name)
+            for area1, centroid1 in centroids.items():
+                logging.debug("area1: %s", area1)
+                for area2, centroid2 in centroids.items():
+                    logging.debug("area2: %s", area2)
+                    area_distance_matrix[map_name][area1][area2][dist_type][
+                        "centroid"
+                    ] = tile_distance_matrix[dist_type][map_name][str(centroid1)][
+                        str(centroid2)
+                    ]
+                    area_distance_matrix[map_name][area1][area2][dist_type][
+                        "representative_point"
+                    ] = tile_distance_matrix[dist_type][map_name][str(reps[area1])][
+                        str(reps[area2])
+                    ]
+                    connections = []
+                    for sub_area1 in area_mapping[area1]:
+                        for sub_area2 in area_mapping[area2]:
+                            connections.append(
+                                tile_distance_matrix[dist_type][map_name][
+                                    str(sub_area1)
+                                ][str(sub_area2)]
+                            )
+                    area_distance_matrix[map_name][area1][area2][dist_type][
+                        "median_dist"
+                    ] = median(connections)
+    if save:
+        with open(json_path, "w", encoding="utf8") as json_file:
+            json.dump(area_distance_matrix, json_file)
+    return area_distance_matrix
+
+
+def generate_centroids(map_name):
+    """For each region in the given map calculates the centroid and a representative point and finds the closest tile for each
+
+    Args:
+        map_name (string): Name of the map for which to calculate the centroids
+
+    Returns:
+        Tuple of dictionaries containing the centroid and representative tiles for each region of the map
+    """
+    centers = {}
+    area_points = defaultdict(list)
+    hulls = {}
+    centers = {}
+    area_points = defaultdict(list)
+    z_s = defaultdict(list)
+    center_z = {}
+    area_ids_cent = {}
+    area_ids_rep = {}
+    my_centroid = {}
+    rep_point = {}
+    for a in NAV[map_name]:
+        area = NAV[map_name][a]
+        cur_x = []
+        cur_y = []
+        cur_x.append(area["southEastX"])
+        cur_x.append(area["northWestX"])
+        cur_y.append(area["southEastY"])
+        cur_y.append(area["northWestY"])
+        z_s[area["areaName"]].append(area["northWestZ"])
+        z_s[area["areaName"]].append(area["southEastZ"])
+        for x, y in itertools.product(cur_x, cur_y):
+            area_points[area["areaName"]].append((x, y))
+    for area in area_points:
+        points_array = np.array(area_points[area])
+        hull = np.array(stepped_hull(area_points[area]))
+        hulls[area] = hull
+        centers[area] = points_array.mean(axis=0)
+        center_z[area] = mean(z_s[area])
+        my_centroid[area] = np.array(Polygon(hull).centroid)
+        rep_point[area] = np.array(Polygon(hull).representative_point())
+        area_ids_cent[area] = find_closest_area(
+            map_name, list(my_centroid[area]) + [center_z[area]]
+        )["areaId"]
+        area_ids_rep[area] = find_closest_area(
+            map_name, list(rep_point[area]) + [center_z[area]]
+        )["areaId"]
+    return area_ids_cent, area_ids_rep
+
+
+def main(args):
+    """Uses awpy and the extension functions defined in this module to plots player positions in 1 or 10 rounds, position tokens in 1 round
+    and each maps nav mesh and named areas."""
+    parser = argparse.ArgumentParser("Analyze the early mid fight on inferno")
+    parser.add_argument(
+        "-d", "--debug", action="store_true", default=False, help="Enable debug output."
+    )
+    parser.add_argument(
+        "-l",
+        "--log",
+        default=r"D:\CSGO\ML\CSGOML\logs\nav_tests.log",
+        help="Path to output log.",
+    )
+    options = parser.parse_args(args)
+
+    if options.log == "None":
+        options.log = None
+    if options.debug:
+        logging.basicConfig(
+            filename=options.log,
+            encoding="utf-8",
+            level=logging.DEBUG,
+            filemode="w",
+            format="%(asctime)s %(levelname)-8s %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+    else:
+        logging.basicConfig(
+            filename=options.log,
+            encoding="utf-8",
+            level=logging.INFO,
+            filemode="w",
+            format="%(asctime)s %(levelname)-8s %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+
+    # with open("D:\CSGO\ML\CSGOML\data\\tile_dist_matrix.json") as f:
+    #     tiles_matrix = json.load(f)
+    with open(
+        "D:\\CSGO\\ML\\CSGOML\\data\\area_dist_matrix.json", encoding="utf8"
+    ) as f:
+        area_matrix = json.load(f)
+
+    logging.info(area_matrix["de_dust2"]["ExtendedA"]["CTSpawn"])
+    logging.info(area_matrix["de_dust2"]["CTSpawn"]["ExtendedA"])
+
+    centroids, reps = generate_centroids("de_dust2")
+    graph = area_distance(
+        "de_dust2", centroids["ExtendedA"], centroids["CTSpawn"], dist_type="graph"
+    )
+    geodesic = area_distance(
+        "de_dust2", centroids["ExtendedA"], centroids["CTSpawn"], dist_type="geodesic"
+    )
+    plot_path("de_dust2", graph, geodesic)
+    graph = area_distance(
+        "de_dust2", centroids["CTSpawn"], centroids["ExtendedA"], dist_type="graph"
+    )
+    geodesic = area_distance(
+        "de_dust2", centroids["CTSpawn"], centroids["ExtendedA"], dist_type="geodesic"
+    )
+    plot_path("de_dust2", graph, geodesic)
+    graph = area_distance(
+        "de_dust2", reps["ExtendedA"], reps["CTSpawn"], dist_type="graph"
+    )
+    geodesic = area_distance(
+        "de_dust2", reps["ExtendedA"], reps["CTSpawn"], dist_type="geodesic"
+    )
+    plot_path("de_dust2", graph, geodesic)
+    graph = area_distance(
+        "de_dust2", reps["CTSpawn"], reps["ExtendedA"], dist_type="graph"
+    )
+    geodesic = area_distance(
+        "de_dust2", reps["CTSpawn"], reps["ExtendedA"], dist_type="geodesic"
+    )
+    plot_path("de_dust2", graph, geodesic)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
