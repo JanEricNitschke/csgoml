Cleaned up vectorized area matrix calculation and fixed test.

csgoml/trajectories/trajectory_clusterer.py
@@ -49,6 +49,7 @@ from typing import Literal
 
 import matplotlib.pyplot as plt
 import numpy as np
+import numpy.typing as npt
 from awpy.data import AREA_DIST_MATRIX, NAV, PLACE_DIST_MATRIX
 from awpy.types import DistanceType
 from numba import typed, types
@@ -555,7 +556,7 @@ class TrajectoryClusterer:
 
     def get_compressed_area_dist_matrix(
         self,
-    ) -> tuple[types.int64, dict[types.int64, types.float64]]:
+    ) -> tuple[npt.NDArray, dict[types.int64, types.float64]]:
         """Generates a compressed area distance matrix.
 
         Args:
@@ -659,8 +660,6 @@ class TrajectoryClusterer:
                     return matching[141] if int(x) not in matching else matching[int(x)]
 
                 clustering_array = np.vectorize(get_matching)(clustering_array)
-                logging.info(clustering_array.shape)
-                clustering_array = np.squeeze(clustering_array, axis=-1)
                 logging.info(clustering_array.shape)
                 precomputed = get_traj_matrix_area(
                     precompute_array=clustering_array,

csgoml/utils/nav_utils.py
@@ -307,9 +307,8 @@ def trajectory_distance(
     )
 
 
-# @njit
 # @np.vectorize
-@njit
+# @njit
 def compute_dtw(
     trajectory_array_1: np.ndarray,
     trajectory_array_2: np.ndarray,
@@ -499,6 +498,10 @@ def get_traj_matrix_area(
     Returns:
         Numpy array of distances between trajectories
     """
+    # Reduce Shape of precompute array is:
+    # N_rounds, N_times, N_teams, N_players, N_features
+    # With N_features always being 1 here. So just squeeze that away at the start.
+    precompute_array = np.squeeze(precompute_array, axis=-1)
     permutations = np.array(
         list(itertools.permutations(range(precompute_array.shape[-1])))
     )

tests/test_nav_utils.py
@@ -281,29 +281,24 @@ class TestNavUtils:
                 [[[[3]]], [[[4]]], [[[5]]]],
             ]
         )
-        d1_type = types.DictType(types.int64, types.float64)
-        dist_matrix = typed.Dict.empty(types.int64, d1_type)
-        for i in range(1, 6):
-            if (i) not in dist_matrix:
-                dist_matrix[i] = typed.Dict.empty(
-                    key_type=types.int64,
-                    value_type=types.float64,
-                )
-            for j in range(1, 6):
-                dist_matrix[i][j] = float(abs(i - j))
+        dist_matrix = np.zeros((6, 6))
+        for i, j in itertools.product(range(1, 6), range(1, 6)):
+            dist_matrix[i, j] = float(abs(i - j))
         target_precomputed = np.zeros((len(to_precompute), len(to_precompute)))
         target_precomputed[0][1] = target_precomputed[1][0] = 1.0
         target_precomputed[0][2] = target_precomputed[2][0] = 2.0
         target_precomputed[1][2] = target_precomputed[2][1] = 1.0
         calc_precomputed = get_traj_matrix_area(to_precompute, dist_matrix, dtw=False)
-        assert (target_precomputed == calc_precomputed).all()
+        print(calc_precomputed.shape)
+        print(calc_precomputed)
         assert target_precomputed.shape == calc_precomputed.shape
+        assert (target_precomputed == calc_precomputed).all()
         calc_precomputed = get_traj_matrix_area(to_precompute, dist_matrix, dtw=True)
         target_precomputed[0][1] = target_precomputed[1][0] = 2 / 3
         target_precomputed[0][2] = target_precomputed[2][0] = 2.0
         target_precomputed[1][2] = target_precomputed[2][1] = 2 / 3
-        assert (target_precomputed == calc_precomputed).all()
         assert target_precomputed.shape == calc_precomputed.shape
+        assert (target_precomputed == calc_precomputed).all()
 
     def test_get_traj_matrix_token(self):
         """Tests get_traj_matrix_token."""